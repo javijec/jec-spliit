ef069a449e60a96c621f649ee974d8178d89cc1a
JaViJeC <javijec@gmail.com>
2026-04-27 21:35:58 -0300

    Add loading states for group subroutes

---
+++ src/app/groups/[groupId]/balances/loading.tsx
@@ -0,0 +1,5 @@
+import { GroupSectionLoading } from '@/components/route-feedback'
+
+export default function GroupBalancesLoading() {
+  return <GroupSectionLoading rows={4} />
+}
---
+++ src/app/groups/[groupId]/summary/loading.tsx
@@ -0,0 +1,5 @@
+import { GroupSectionLoading } from '@/components/route-feedback'
+
+export default function GroupSummaryLoading() {
+  return <GroupSectionLoading rows={3} />
+}
